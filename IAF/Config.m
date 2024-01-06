9482f09d93c20093cfb9ec56ba6e4d60de9d46a5
Jonas Heinemann <jonas.heinemann2@iee-extern.fraunhofer.de>
2024-01-06 16:02:35 +0100

    8608 Wörter Simulation aufgeräumt und hinzugefügt

---
+++ IAF/Config.m
@@ -0,0 +1,68 @@
+%%Namene
+Model.Name = "IAF_model";
+clear;
+%% Grid
+Netz.Frequenz = 50;             %Hz
+Netz.SpannungLL = 617;           %Volt
+Netz.SpannungPeak = Netz.SpannungLL /sqrt(3)*sqrt(2);
+Phaseshift = 0;
+
+Netz.R1 = 0;                    %Reihenwiderstand von Netz
+Netz.R2 = 50;                   %Reihenwiderstand zu den Filterkondensatoren
+Netz.L1 = 0;                    %Reiheninduktivität des Netzes
+Netz.C1 = 0;                    %Kondensator 1. Filter
+
+%% Electrolyseur
+Elektrolyseur.Spannung = 680;
+Elektrolyseur.Leistung = 200e3;
+Elektrolyseur.Strom = Elektrolyseur.Leistung/Elektrolyseur.Spannung;
+Elektrolyseur.R1 = Elektrolyseur.Spannung / Elektrolyseur.Strom;
+
+
+%% Thermal
+HeatSinkTemperatur = 100;
+InitialTemperaturDelta = 0;
+
+%% Die schnellen Schalter
+T_RgOn = 3.6;
+T_RgOff = 1;
+T_TIM = 0;
+T_Ron=1e-3;
+T_Vf=3;
+
+%% Input Voltage Selector
+IVS_RgOn = 3.6;
+IVS_RgOff = 1;
+IVS_TIM = 0;
+
+%% Dioden Gleichrichter
+D_TIM = 0;
+D_Ron=1e-3;
+D_Vf=1;
+
+%% Buck
+T_Buck_RgOn = 3.6;
+T_Buck_RgOff = 1;
+T_Buck_TIM = 0;
+
+IAF.DCDC.Schaltfrequenz = 20e3;
+IAF.Reglerfrequenz = 50e3;
+
+%% Berechnung der Induktivität des DC DC Wanders
+
+IAF.L2 = 136e-6;
+
+%% Netzeingang
+IAF.C1 = 50e-6;
+IAF.L1 = 1e-6;%Netz
+IAF.C2 = 1e-3; %Electrolyseur
+IAF.C3 = 5e-5; %Zwischenkreis
+IAF.R1 = 1e-3; %Kondensator Innenwiderstand
+
+IAF.Schaltfrequenz = 20000; % original 10000
+IAF.Stromregler.SampleTime = 1/100000;
+IAF.Deadtime = 500e-9; %
+
+%%Induktivität des IVS
+IAF.L3 = 302.2e-6; 
+
